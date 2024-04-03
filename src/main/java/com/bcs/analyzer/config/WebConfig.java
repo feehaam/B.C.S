1dca104ac54a73de657791068cfd15ef3483c2a6
feehaam <mdfeeham@gmail.com>
2024-04-03 18:42:56 +0600

    Front end url updated

--- src/main/java/com/bcs/analyzer/config/WebConfig.java
+++ src/main/java/com/bcs/analyzer/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://bcs-hunt.netlify.app") // Update this with your frontend URL
+                .allowedOrigins("http://bcs-hunt.netlify.app") // Update this with your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
