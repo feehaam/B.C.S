1c051c36adbc66fc053b9cf63b2895e837e82cd4
feehaam <mdfeeham@gmail.com>
2024-04-03 18:58:32 +0600

    Front end url updated

---
+++ src/main/java/com/bcs/analyzer/config/AccessOriginConfig.java
@@ -0,0 +1,28 @@
+package com.bcs.analyzer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class AccessOriginConfig {
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://bcs-hunt.netlify.app/");
+        config.addAllowedOrigin("https://bcs-hunt.netlify.app/");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
